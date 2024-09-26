dfff444d33076b59f11da8018192d22ec9e19866
hoon <hh02021@naver.com>
2024-09-26 11:46:14 +0900

    layout.tsx fix

---
+++ src/app/add/page.tsx
@@ -0,0 +1,11 @@
+
+
+const page = () => (
+  <div>
+     <main>
+		    <h1>New Bookmark Form</h1>
+		 </main>   
+  </div>
+);
+
+export default page;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,9 @@
-import NavBar from '@/components/NavBar';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
 export default function Home() {
   return (
-   
     <div>
-      <NavBar></NavBar>
       <main>
         <h1>Welcome to Bookmarker</h1>
       </main>
-  </div>
+    </div>
   );
 }
